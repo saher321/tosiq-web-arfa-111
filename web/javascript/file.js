1d8d8458368cc968fea15e63a9e38bf5a8c23fb6
pny <pnymeet@gmail.com>
2026-05-05 10:49:40 +0500

    basic js

---
+++ web/javascript/file.js
@@ -0,0 +1,19 @@
+// DATA TYPES
+// number: 1234, 3.543
+// string: "World", "W"
+// object user = { key: value, key: value }
+var userA = {
+    name: "Ali",
+    age: 23,
+    email: "ali@gmail.com"
+}
+// console.log(typeof userA)
+// array: [{age: 23}, "world", 34, "W"]
+
+// indexes   0        1         2
+var list = ["Black", "Purple", "White"]
+// console.log(list[1])
+// boolean : true, false
+function show() {
+    
+}
---
+++ web/javascript/program1.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = -2;
+// document.write("Sum is: ", a+b)
+
+var fah = 12;
+var c = (fah - 32) * 5/9;
+console.log("Temperature in Celc: ", c.toFixed(2))
