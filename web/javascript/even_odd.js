371e78c6cf8095581d58ec28506179775be0d50f
pny <pnymeet@gmail.com>
2026-05-06 11:44:22 +0500

    6 may assignment shopping cart

--- web/javascript/even_odd.js
+++ web/javascript/even_odd.js
@@ -1,10 +1,24 @@
+// Comparison operators
+// !        Not operator
+// !=       Not qual to
+// >        Greater then
+// <        Less then
+// >=       Greater then and equal to
+// <=       Less the and equal to
+// ||       OR operator
+// &&       AND operator
+// ==       Match the values        if ( 2 != "2" ) => true
+// ===      Match the values and    if ( 2 === "2" ) => false
+
 function findEvenOdd () {
     var no1 = document.getElementById("number1").value
     var result = document.getElementById("result");
 
-    if (no1 % 2 == 0) { // when condition will be true
+    if (no1 % 2 == 0 && no2 % 2 == 0) { // when condition will be true
         result.innerText = "Given number is Even";
     } else { // when condition will be false
         result.innerText = "Given number is Odd";
     }
 }
+
+// find positive or negative number
