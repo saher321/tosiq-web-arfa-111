5fdda1a4fee9eef96623cb2a5633c6cdeb69f283
pny <pnymeet@gmail.com>
2026-05-11 11:15:45 +0500

    loop

---
+++ web/javascript/loop.js
@@ -0,0 +1,12 @@
+// variable declaration, variable initialization
+// condition
+// incr, decr
+var i;
+for(i = 1; i <= 10; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+// i = 1 => 1 :: 1 iteration
+// i = 2 => 
