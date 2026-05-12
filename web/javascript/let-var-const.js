d26958ac8269c0de674b0795f0ac0f1f4ec56c2a
pny <pnymeet@gmail.com>
2026-05-12 10:48:36 +0500

    let  var const

---
+++ web/javascript/let-var-const.js
@@ -0,0 +1,18 @@
+var age = 34
+var age = 12
+console.log(age)
+// var:: variable will be redeclared and will be re-initialized
+
+console.log(name)
+let name = "Talha"
+name = "Usman"
+// let:: variable will not be redeclared but will be re-initialized
+
+// hoisting: a JavaScript mechanism where variable and 
+// function declarations are moved to the top 
+// of their scope (global or function) before code execution
+
+const email = "email@gmail.com";
+// isActive = false;
+console.log(isActive)
+// const:: variable will not be  redeclared or re-initialized
