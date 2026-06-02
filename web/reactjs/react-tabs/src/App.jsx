c5ccb6ecd9179df35ad0f4ac044797735241b6a6
tosiq <pnymeet@gmail.com>
2026-06-02 11:34:20 +0500

    add to cart functionality

--- web/reactjs/react-tabs/src/App.jsx
+++ web/reactjs/react-tabs/src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
 const App = () => {
   const [ value, setValue ] = useState(10)
+
+  const [ changeQTY, setChangeQTY ] = useState(1)
   // const [ value, setValue ] = useState("Name")
   // const [ value, setValue ] = useState(true)
   // const [ value, setValue ] = useState({ name: "Ali" })
@@ -9,10 +12,32 @@ const App = () => {
   const handleChange = () => {
     setValue(5)
   }
+
+  const decQTY = () => {
+    if ( changeQTY <= 1 ) {
+      return;
+    }
+    setChangeQTY(changeQTY - 1)
+  }
+  const inQTY = () => {
+    if ( changeQTY == 10 ) {
+      toast.error("Limit reached")
+      return;
+
+    }
+    setChangeQTY(changeQTY + 1)
+  }
   return (
     <div>
       App : {value} | 
       <button onClick={handleChange}>Change value</button>
+      <hr />
+
+      <div>
+        <button onClick={decQTY}> &mdash; </button> {" "}
+        <strong> {changeQTY} </strong> {" "}
+        <button onClick={inQTY}> + </button>
+      </div>
     </div>
   )
 }
