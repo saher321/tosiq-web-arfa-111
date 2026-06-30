b4055d0725a17cef881c9bdcc93a863bdb8dab6f
Tosiq <pnymeet@gmail.com>
2026-06-30 11:23:48 +0500

    backend

--- web/mernstack/app1/server/index.js
+++ web/mernstack/app1/server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello from node js")
+import express from 'express'
+
+const app = express();
+
+// http://localhost:5000/
+const colors = [ "Purple", "Orange", "Brown", "Yellow", "Red"]
+
+app.get('/colors', (req, res) => {
+  return res.send({
+    status: true,
+    colors
+  })
+})
+app.get('/', (req, res) => {
+  return res.send({
+    status: true,
+    message: "Server is running..."
+  })
+})
+
+app.listen(5000, () => {
+  console.log("Server is started http://localhost:"+ 5000 )
+})
