f43c5a55e48b40bd12b8fb67beb557a23f59ccf5
Tosiq <pnymeet@gmail.com>
2026-07-13 23:34:23 -0700

    dynamic routing, link

--- web/mernstack/user-app/ui/src/pages/UserDetails.jsx
+++ web/mernstack/user-app/ui/src/pages/UserDetails.jsx
@@ -1,14 +1,250 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { useParams } from 'react-router'
 import WebLayout from '../layouts/WebLayout'
+import axios from 'axios'
+import { USER_API_URL } from '../utils/api.js'
+import { HiPencil } from "react-icons/hi";
 
 const UserDetails = () => {
+    const [user, setUser] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     const params = useParams()
+    const getSingleUser = async () => {
+        try {
+            setIsLoading(true)
+            const response = await axios.get(USER_API_URL)
+            if (response.data.status == true && response.data.users.length > 0) {
+                let userDetails = response.data.users.find((usr) => usr.id == params.id)
+                setUser(userDetails)
+            }
+        } catch (error) {
+            console.log("Error: ", error)
+            setIsLoading(false)
+        }
 
+    }
+
+    useEffect(() => {
+        getSingleUser()
+    }, [])
     return (
         <WebLayout>
-            User detail page &mdash; #{params.id}
+            <div className='grid grid-cols-12 gap-4 mb-5'>
+                <div className='col-span-12 bg-white p-5 rounded-md shadow'>
+                    <div className='flex items-center gap-5'>
+                        <div>
+                            <img src={user.image} className='rounded-full h-16 w-16' alt="" />
+                        </div>
+                        <div>
+                            <span className='font-bold block'>{user.firstName + " " + user.lastName}</span>
+                            <small>{user.company?.title}</small> | <small>{user.company?.department}</small>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-span-6 bg-white p-5 rounded-md shadow h-fit'>
+                    <div className='pb-1 flex border-0 border-b border-gray-200 items-center justify-between'>
+                        <div className='font-bold'>Personal Information</div>
+                        <HiPencil size={24} />
+                    </div>
+                    <div className='personal-information'>
+                        <table width={"100%"}>
+                            <tr>
+                                <td>
+                                    <label>Gender</label>
+                                    <span className='font-bold block capitalize'>{user.gender}</span>
+                                </td>
+                                <td>
+                                    <label>Date of birth</label>
+                                    <span className='font-bold block capitalize'>{user.birthDate}</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Identify code</label>
+                                    <span className='font-bold block capitalize'>{user.ein}</span>
+                                </td>
+                                <td>
+                                    <label>Hometown</label>
+                                    <span className='font-bold block capitalize'>{user.address?.city}</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Nationality</label>
+                                    <span className='font-bold block capitalize'>{user.address?.country}</span>
+                                </td>
+                                <td>
+                                    <label>Blood group</label>
+                                    <span className='font-bold block capitalize'>{user.bloodGroup}</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Role</label>
+                                    <span className='font-bold block capitalize'>{user.role}</span>
+                                </td>
+                                <td>
+                                    <label>Age</label>
+                                    <span className='font-bold block capitalize'>{user.age}</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}>
+                                    <label>Permanent address</label>
+                                    <span className='font-bold block capitalize'>
+                                        {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}>
+                                    <label>Current address</label>
+                                    <span className='font-bold block capitalize'>
+                                        {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                    </span>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div className='col-span-6'>
+                    <div className="grid grid-cols-12 gap-4">
+                        <div className="col-span-12 bg-white p-5 rounded-md shadow">
+                            <div className='pb-1 flex border-0 border-b border-gray-200 items-center justify-between'>
+                                <div className='font-bold'>Company Details</div>
+                                <HiPencil size={24} />
+                            </div>
+                            <div className='personal-information'>
+                                <table width={"100%"}>
+                                    <tr>
+                                        <td>
+                                            <label>Gender</label>
+                                            <span className='font-bold block capitalize'>{user.gender}</span>
+                                        </td>
+                                        <td>
+                                            <label>Date of birth</label>
+                                            <span className='font-bold block capitalize'>{user.birthDate}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Identify code</label>
+                                            <span className='font-bold block capitalize'>{user.ein}</span>
+                                        </td>
+                                        <td>
+                                            <label>Hometown</label>
+                                            <span className='font-bold block capitalize'>{user.address?.city}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Nationality</label>
+                                            <span className='font-bold block capitalize'>{user.address?.country}</span>
+                                        </td>
+                                        <td>
+                                            <label>Blood group</label>
+                                            <span className='font-bold block capitalize'>{user.bloodGroup}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Role</label>
+                                            <span className='font-bold block capitalize'>{user.role}</span>
+                                        </td>
+                                        <td>
+                                            <label>Age</label>
+                                            <span className='font-bold block capitalize'>{user.age}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2}>
+                                            <label>Permanent address</label>
+                                            <span className='font-bold block capitalize'>
+                                                {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2}>
+                                            <label>Current address</label>
+                                            <span className='font-bold block capitalize'>
+                                                {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div className="col-span-12 bg-white p-5 rounded-md shadow">
+                            <div className='pb-1 flex border-0 border-b border-gray-200 items-center justify-between'>
+                                <div className='font-bold'>Account Information</div>
+                                <HiPencil size={24} />
+                            </div>
+                            <div className='personal-information'>
+                                <table width={"100%"}>
+                                    <tr>
+                                        <td>
+                                            <label>Gender</label>
+                                            <span className='font-bold block capitalize'>{user.gender}</span>
+                                        </td>
+                                        <td>
+                                            <label>Date of birth</label>
+                                            <span className='font-bold block capitalize'>{user.birthDate}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Identify code</label>
+                                            <span className='font-bold block capitalize'>{user.ein}</span>
+                                        </td>
+                                        <td>
+                                            <label>Hometown</label>
+                                            <span className='font-bold block capitalize'>{user.address?.city}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Nationality</label>
+                                            <span className='font-bold block capitalize'>{user.address?.country}</span>
+                                        </td>
+                                        <td>
+                                            <label>Blood group</label>
+                                            <span className='font-bold block capitalize'>{user.bloodGroup}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>Role</label>
+                                            <span className='font-bold block capitalize'>{user.role}</span>
+                                        </td>
+                                        <td>
+                                            <label>Age</label>
+                                            <span className='font-bold block capitalize'>{user.age}</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2}>
+                                            <label>Permanent address</label>
+                                            <span className='font-bold block capitalize'>
+                                                {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan={2}>
+                                            <label>Current address</label>
+                                            <span className='font-bold block capitalize'>
+                                                {user.address?.address + ", " + user.address?.city + ", " + user.address?.country}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </WebLayout>
     )
 }
--- web/mernstack/user-app/ui/src/pages/Users.jsx
+++ web/mernstack/user-app/ui/src/pages/Users.jsx
@@ -4,20 +4,24 @@ import axios from "axios"
 import { USER_API_URL } from '../utils/api.js'
 import UserItem from '../components/UserItem.jsx'
 import WebLayout from '../layouts/WebLayout.jsx'
+import { HashLoader } from 'react-spinners'
 
 const Users = () => {
   const [users, setUsers] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
 
   const getAllUser = async () => {
     try {
+      setIsLoading(true)
       const response = await axios.get(USER_API_URL)
       console.log(response.data.users)
       if (response.data.status == true && response.data.users.length > 0) {
         setUsers(response.data.users)
-
+        setIsLoading(false)
       }
     } catch (error) {
       console.log("Error: ", error)
+      setIsLoading(false)
     }
 
   }
@@ -31,14 +35,18 @@ const Users = () => {
       <div className="grid grid-cols-12 gap-3">
         {
           users.length == 0 ?
-            <div>No records</div> :
-            users.map((user, i) => {
-              return (
-                <div key={i} className='col-span-4'>
-                  <UserItem user={user} />
-                </div>
-              )
-            })
+            <div>No records</div> : 
+          isLoading ? 
+            <div className='h-screen grid items-center place-items-center '>
+              <HashLoader />
+            </div> :
+          users.map((user, i) => {
+            return (
+              <div key={i} className='col-span-4'>
+                <UserItem user={user} />
+              </div>
+            )
+          })
         }
       </div>
     </WebLayout>
