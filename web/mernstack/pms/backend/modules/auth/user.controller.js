19d60652ebaa80689356ff5583435329f2159a7d
Tosiq <pnymeet@gmail.com>
2026-07-21 23:00:00 -0700

    .

--- web/mernstack/pms/backend/modules/auth/user.controller.js
+++ web/mernstack/pms/backend/modules/auth/user.controller.js
@@ -4,17 +4,10 @@ export const getAllUsers = async (req, res) => {
 
     try {
         const users = await User.find({})
-        if (users) {
-            return res.send({
-                status: true,
-                users: users
-            })
-        } else {
-            return res.send({
-                status: false,
-                message: "Users not found or maybe removed"
-            })
-        }
+        return res.send({
+            status: true,
+            users
+        })
     } catch (error) {
         throw new Error(error)
     }
